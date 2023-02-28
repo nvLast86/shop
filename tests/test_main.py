2154f6c60cdb0d665b05fad6042d2953e26fc6ef
nvLast86 <n.lastenko@gmail.com>
2023-02-28 20:53:06 +0600

    add docstrings

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,9 +6,17 @@ def some_item():
     return Item('test_position', 100, 10)
 
 def test_calculate_total_price(some_item):
+    """
+    Создаем тестовый экземпляр класса с ценой за единицу в 100 и количестве 10.
+    Метод экземпляра calculate_total_price() должен вернуть 1000
+    """
     assert some_item.calculate_total_price() == 1000
 
 def test_apply_discount(some_item):
+    """
+    Создаем тестовый экземпляр класса с ценой за единицу в 100 и количестве 10.
+    Метод экземпляра apply_discount() должен вернуть новую цену за единицу в 50
+    """
     Item.pay_rate = 0.5
     assert some_item.apply_discount() == 50
 
