37b1e6dee2391841dfff8f77e1c1f1360c46503e
nvLast86 <n.lastenko@gmail.com>
2023-03-09 12:20:19 +0600

    The part of code with creating class' examples has been moved to main.py

--- utils/item.py
+++ utils/item.py
@@ -35,18 +35,6 @@ class Item:
         return self.item_price
 
 
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
-
-Item.pay_rate = 0.8  # устанавливаем новый уровень цен
-item1.apply_discount()
-print(item1.item_price)
-print(item2.item_price)
-
-
-print(Item.all)
 
 
