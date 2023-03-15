1cda5da34520f5239f7009137b242ef5310daf74
nvLast86 <n.lastenko@gmail.com>
2023-03-15 12:08:14 +0600

    main.py have been changed according to task#3

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from utils.item import Item
 import os
 
+def main():
+    item1 = Item("Смартфон", 10000, 20)
+    Item('Смартфон', 10000, 20)
+    print(item1)
 
+
+"""
 def main():
     path = os.sep.join(['files', 'items.csv'])
 
-    item = Item('Телефон', 10000, 5)
-    item.item_name = 'Смартфон'
-    print(item.item_name)
+    #item = Item('Телефон', 10000, 5)
+    #item.item_name = 'Смартфон'
+    #print(item.item_name)
 
     #item.item_name = 'СуперСмартфон'
 
@@ -20,7 +26,7 @@ def main():
     print(Item.is_number_integer(5))
     print(Item.is_number_integer(5.0))
     print(Item.is_number_integer(5.5))
-
+"""
 
 if __name__ == "__main__":
     main()
