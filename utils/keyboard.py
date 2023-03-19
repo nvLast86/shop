a6f36ce80597c81b7d56581c7172b29de4be0871
nvLast86 <n.lastenko@gmail.com>
2023-03-19 17:22:29 +0600

    class Mixinlang has been added

--- utils/keyboard.py
+++ utils/keyboard.py
@@ -5,3 +5,18 @@ class Keyboard(Item, Mixinlang):
 
 
 class Mixinglang:
+
+    def __init__(self, *args):
+        self.__language = 'EN'
+        super().__init__(*args)
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == 'RU':
+            self.__language = 'EN'
+        else:
+            self.__language = 'RU'
+
