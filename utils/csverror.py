fb12974e32cbb9fb06c68f565003c63c135e423c
nvLast86 <n.lastenko@gmail.com>
2023-03-24 11:23:37 +0600

    The code of class InstantiateCSVError(Exception) has been added

--- utils/csverror.py
+++ utils/csverror.py
@@ -2,3 +2,8 @@ class InstantiateCSVError(Exception):
     """
     Класс исключение для ошибок, связанных с пореждением файла
     """
+    def __init__(self, *args):
+        self.message = args[0] if args else 'Файл поврежден'
+
+    def __str__(self):
+        return self.message
